97fc9ee1b2294ce9ccdebca46957e3bd5b5478a7
Cliff Wu <z800201002005@gmail.com>
2022-08-01 19:21:25 +0900

    Read data from serial port and send it to host

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::time::Duration;
+use std::str;
+use std::io;
 
 fn main() {
     let ports = serialport::available_ports().expect("No ports found!");
@@ -7,9 +9,26 @@ fn main() {
     for p in ports {
         println!("{}", p.port_name);
     }
+    println!("Open: /dev/ttyS0");
     let mut port = serialport::new("/dev/ttyS0", 115200)
         .timeout(Duration::from_millis(10))
         .open().expect("Failed to open port");
-    let output = "rust-serial-echo v0.1.0\r\n".as_bytes();
-    port.write(output).expect("Write failed!");
+    let version = "rust-serial-echo v0.1.0\r\n".as_bytes();
+    port.write(version).expect("Write failed!");
+    println!("Echo server started!");
+    let mut serial_buf: Vec<u8> = vec![0; 1024];
+    loop {
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(length) => {
+                let data = match str::from_utf8(&serial_buf) {
+                    Ok(v) => v,
+                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                };
+                println!("Received data: {}, length: {}", data, length);
+                port.write(&serial_buf[0..length]).expect("Write failed!");
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
 }
