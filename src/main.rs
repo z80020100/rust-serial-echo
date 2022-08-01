fdf5d11d01662979bec2a3f9d52786d0519d63aa
Cliff Wu <z800201002005@gmail.com>
2022-08-01 18:18:46 +0900

    Send version info to host

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 fn main() {
     let ports = serialport::available_ports().expect("No ports found!");
     // println!("Available ports: {:?}", ports);
@@ -5,4 +7,9 @@ fn main() {
     for p in ports {
         println!("{}", p.port_name);
     }
+    let mut port = serialport::new("/dev/ttyS0", 115200)
+        .timeout(Duration::from_millis(10))
+        .open().expect("Failed to open port");
+    let output = "rust-serial-echo v0.1.0\r\n".as_bytes();
+    port.write(output).expect("Write failed!");
 }
